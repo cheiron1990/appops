a29b64ed43be054455384ae941aecdc456000f77
Cheiron Zhang <cheironde@gmail.com>
2016-10-26 11:57:05 +0800

    save

--- main.js
+++ main.js
@@ -12,12 +12,10 @@ const exec = require('child_process').exec
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-app.setName('Appops')
-
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800, 
+    width: 800,
     height: 600,
     title: 'Appops',
     titleBarStyle: 'hidden-inset'
@@ -39,117 +37,118 @@ function createWindow () {
 }
 
 function getAppList () {
-  return new Promise(function(resolve, reject){
-    exec('adb shell pm list packages -3', function(err, stdout, stderr){
+  return new Promise((resolve, reject) => {
+    exec('adb shell pm list packages -3', (err, stdout, stderr) => {
       if (err) {
-        reject(err);
-      }else{
-        let arr = stdout.split('\npackage:');
-        arr[0] = arr[0].slice(8);
-        let apps = [];
-        arr.forEach(function(item, index, arr){
-          let obj = {};
-          obj.packageName = item;
-          apps.push(obj);
-        });
-        resolve(apps);
+        reject(err)
+      } else {
+        let arr = stdout.split('\npackage:')
+        arr[0] = arr[0].slice(8)
+        let apps = []
+        arr.forEach((item, index, arr) => {
+          let obj = {}
+          obj.packageName = item
+          apps.push(obj)
+        })
+        resolve(apps)
       }
-    });
+    })
   })
 }
 
 function getAppPermissions (packageName) {
-  return new Promise(function(resolve, reject){
-    exec('adb shell appops get ' + packageName, function(err, stdout, stderr){
-      if(err){
-        reject(err);
-      }else{
-        let arr = stdout.split('\n');
-        arr.length -= 1;
-        if(arr[0].indexOf('No operations') != -1){
-          arr.length = 0;
-        }else{
-          arr.forEach(function(item, index, arr){
-            kv = item.slice(0, item.indexOf(';')).split(': ');
+  return new Promise((resolve, reject) => {
+    exec('adb shell appops get ' + packageName, (err, stdout, stderr) => {
+      if (err) {
+        reject(err)
+      } else {
+        let arr = stdout.split('\n')
+        arr.length -= 1
+        if (arr[0].indexOf('No operations') !== -1) {
+          arr.length = 0
+        } else {
+          arr.forEach((item, index, arr) => {
+            let kv = item.slice(0, item.indexOf(';')).split(': ')
             arr[index] = {
               key: kv[0],
               value: kv[1]
-            };
-          });
+            }
+          })
         }
-        let permissions = arr;
-        resolve(permissions);
+        let permissions = arr
+        resolve(permissions)
       }
-    });
-  });
+    })
+  })
 }
 
 function setAppPermission (packageName, permission) {
-  return new Promise(function(resolve, reject){
-    exec('adb shell appops set ' + packageName + ' ' + permission.key + ' ' + permission.value, function(err, stdout, stderr){
-      if(err){
-        reject(err);
-      }else{
-        resolve();
+  return new Promise((resolve, reject) => {
+    exec('adb shell appops set ' + packageName + ' ' + permission.key + ' ' + permission.value, (err, stdout, stderr) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
       }
-    });
-  });
+    })
+  })
 }
 
 function setAppPermissions (packageName, permissions) {
-  permissions.forEach(function(item, index, arr){
-    setAppPermission(packageName, item).then(function(){
-      console.log('s');
-    }, function(err){
-      console.log(err);
-    });
-  });
+  permissions.forEach((item, index, arr) => {
+    setAppPermission(packageName, item).then(() => {
+      console.log('s')
+    }, (err) => {
+      console.log(err)
+    })
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', function(){
-  createWindow();
+app.on('ready', () => {
+  createWindow()
 })
 
-ipcMain.on('getAppList', function(e){
-  getAppList().then(function(apps){
+ipcMain.on('getAppList', (e) => {
+  getAppList().then((apps) => {
     e.sender.send('getAppList', {
       code: 1,
       data: apps
-    });
-  }, function(err){
+    })
+  }, (err) => {
     e.sender.send('getAppList', {
       code: 0,
+      errStr: err.toString(),
       err: err
-    });
-    console.log(err);
-  });
+    })
+    console.log(err)
+  })
 })
 
-ipcMain.on('getAppPermissions', function(e, arg){
-  getAppPermissions(arg).then(function(permissions){
+ipcMain.on('getAppPermissions', (e, arg) => {
+  getAppPermissions(arg).then((permissions) => {
     e.sender.send('getAppPermissions', {
       code: 1,
       packageName: arg,
       permissions: permissions
-    });
-  }, function(err){
+    })
+  }, (err) => {
     e.sender.send('getAppPermissions', {
       code: 0,
       err: err
-    });
-    console.log(err);
-  });
+    })
+    console.log(err)
+  })
 })
 
-ipcMain.on('setAppPermissions', function(e, arg){
-  setAppPermissions(arg.packageName, arg.permissions);
+ipcMain.on('setAppPermissions', (e, arg) => {
+  setAppPermissions(arg.packageName, arg.permissions)
 })
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -157,7 +156,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
--- renderer.js
+++ renderer.js
@@ -2,142 +2,209 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const remote = require('electron').remote;
 const ipcRenderer = require('electron').ipcRenderer;
 
-function renderApp(app){
-	let item = document.createElement('div');
-	item.className = 'app';
-	let btn = document.createElement('a');
-	btn.className = 'app-name'
-	btn.innerHTML = item.id = app.packageName;
-	item.appendChild(btn);
-	btn.addEventListener('click', function(e){
-		let _form = item.getElementsByTagName('form');
-		if(_form.length > 0){
-			for (let i = _form.length - 1; i >= 0; i--) {
-				item.removeChild(_form[i]);
-			}
-			item.className = 'app';
-		}else{
-			ipcRenderer.send('getAppPermissions', app.packageName);
-		}
-	});
-	return item;
-}
+(() => {
+  const commonMethod = {
+    get: function (url) {
+      return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+              resolve(xhr)
+            } else {
+              reject(xhr)
+            }
+          }
+        }
+        xhr.open('get', url, true)
+        xhr.send(null)
+      })
+    }
+  }
+  //
+  function getLabel (packageName) {
+    return new Promise((resolve, reject) => {
+      if (localStorage.getItem(packageName + '_logo') && localStorage.getItem(packageName + '_label')){
+        let logo = localStorage.getItem(packageName + '_logo')
+        let label = localStorage.getItem(packageName + '_label')
+        resolve({
+          logo: logo,
+          label: label
+        })
+      } else {
+        commonMethod.get('https://play.google.com/store/apps/details?id=' + packageName).then((xhr) => {
+          let htmlStr = xhr.response
+          let parser = new DOMParser()
+          let dom = parser.parseFromString(htmlStr, 'text/html')
+          let logo = 'https:' + dom.querySelector('.cover-image').getAttribute('src')
+          let label = dom.querySelector('.id-app-title').innerHTML
+          // save
+          localStorage.setItem(packageName + '_logo', logo)
+          localStorage.setItem(packageName + '_label', label)
+          resolve({
+            logo: logo,
+            label: label
+          })
+        }, (xhr) => {
+          reject(xhr.status)
+        })
+      }
+    })
+  }
+  //
+  function renderApp (app) {
+    let item = document.createElement('div')
+    item.className = 'app'
+    let icon = document.createElement('div')
+    icon.className = 'app-icon'
+    let btn = document.createElement('a')
+    btn.className = 'app-name'
+    btn.innerHTML = item.id = app.packageName
+    item.appendChild(icon)
+    item.appendChild(btn)
+    btn.addEventListener('click', function (e) {
+      let _form = item.getElementsByTagName('form')
+      if (_form.length > 0) {
+        for (let i = _form.length - 1; i >= 0; i--) {
+          item.removeChild(_form[i])
+        }
+        item.className = 'app'
+      } else {
+        ipcRenderer.send('getAppPermissions', app.packageName)
+      }
+    })
+    getLabel(app.packageName).then((obj) => {
+      // set icon
+      icon.style.backgroundColor = 'transparent'
+      icon.style.borderRadius = '0'
+      icon.style.backgroundSize = 'contain'
+      icon.style.backgroundImage = 'url("' + obj.logo + '")'
+      // set label
+      btn.innerHTML = obj.label
+    }, (errCode) => {
+      console.log(errCode)
+    })
+    return item
+  }
+  //
+  function renderAppList (apps) {
+    let container = document.getElementById('appList')
+    apps.forEach(function (item, index, arr) {
+      container.appendChild(renderApp(item))
+    })
+  }
+  //
+  function renderAppPermissions (packageName, permissions) {
+    let container = document.getElementById(packageName)
+    let _form = container.getElementsByTagName('form')
+    if (_form.length > 0) {
+      for (let i = _form.length - 1; i >= 0; i--) {
+        container.removeChild(_form[i])
+      }
+      container.className = 'app'
+    }
+    let form = document.createElement('form')
+    if (permissions.length > 0) {
+      let ul = document.createElement('ul')
+      let permissionNames = []
+      permissions.forEach((item, index, arr) => {
+        permissionNames.push(item.key)
+        let li = document.createElement('li')
+        let label = document.createElement('label')
+        label.innerHTML = item.key
+        label.setAttribute('for', item.key)
+        let valueList = document.createElement('select')
+        valueList.id = valueList.name = item.key
+        let allowOption = document.createElement('option')
+        allowOption.innerHTML = allowOption.value = 'allow'
+        let ignoreOption = document.createElement('option')
+        ignoreOption.innerHTML = ignoreOption.value = 'ignore'
+        let denyOption = document.createElement('option')
+        denyOption.innerHTML = denyOption.value = 'deny'
+        let defaultOption = document.createElement('option')
+        defaultOption.innerHTML = defaultOption.value = 'default'
+        switch (item.value) {
+          case 'allow':
+            allowOption.selected = 'selected'
+            break
+          case 'ignore':
+            ignoreOption.selected = 'selected'
+            break
+          case 'deny':
+            denyOption.selected = 'selected'
+            break
+          default:
+            defaultOption.selected = 'selected'
+        }
+        valueList.appendChild(allowOption)
+        valueList.appendChild(ignoreOption)
+        valueList.appendChild(defaultOption)
+        li.appendChild(label)
+        li.appendChild(valueList)
+        ul.appendChild(li)
+      })
+      let btn = document.createElement('button')
+      btn.type = 'submit'
+      btn.innerHTML = '设置'
+      form.appendChild(ul)
+      form.appendChild(btn)
+      btn.addEventListener('click', (e) => {
+        e.preventDefault()
+        let data = []
+        permissionNames.forEach((item, index, arr) => {
+          let obj = {
+            key: item,
+            value: form[item].value
+          }
+          data.push(obj)
+        })
+        ipcRenderer.send('setAppPermissions', {
+          packageName: packageName,
+          permissions: data
+        })
+      })
+    } else {
+      let tip = document.createElement('p')
+      tip.innerHTML = 'This app has no permission.'
+      form.appendChild(tip)
+    }
+    container.appendChild(form)
+    container.className = 'app open'
+  }
+  //
+  ipcRenderer.on('getAppPermissions', (e, arg) => {
+    if (arg.code) {
+      renderAppPermissions(arg.packageName, arg.permissions)
+    } else {
+      console.log(arg.err)
+    }
+  })
+  //
+  ipcRenderer.on('getAppList', (e, arg) => {
+    if (arg.code) {
+      document.getElementById('err').style.display = 'none'
+      renderAppList(arg.data)
+    } else {
+      document.getElementById('errStr').innerHTML = arg.errStr
+      document.getElementById('errCode').innerHTML = arg.err.code
+      document.getElementById('errCmd').innerHTML = arg.err.cmd
+      document.getElementById('errSignal').innerHTML = arg.err.signal == null ? arg.err.signal : 'null'
+      document.getElementById('errKilled').innerHTML = arg.err.killed
+      document.getElementById('err').style.display = 'block'
+    }
+  })
+  // init
+  ipcRenderer.send('getAppList')
+  // refresh
+  let btnRefresh = document.getElementById('btnRefresh')
+  btnRefresh.addEventListener('click', (e) => {
+    let contents = document.getElementById('appList').childNodes
+    for (let i = contents.length - 1; i >= 0; i--) {
+      document.getElementById('appList').removeChild(contents[i])
+    }
+    ipcRenderer.send('getAppList')
+  })
+})()
 
-function renderAppList(apps) {
-	let container = document.getElementById('appList');
-	apps.forEach(function(item, index, arr){
-		container.appendChild(renderApp(item));
-	});
-}
-
-function renderAppPermissions(packageName, permissions){
-	let container = document.getElementById(packageName);
-	let _form = container.getElementsByTagName('form');
-	if(_form.length > 0){
-		for (let i = _form.length - 1; i >= 0; i--) {
-			container.removeChild(_form[i]);
-		}
-		container.className = 'app';
-	}
-	let form = document.createElement('form');
-	if(permissions.length > 0){
-		let ul = document.createElement('ul');
-		let permissionNames = [];
-		permissions.forEach(function(item, index, arr){
-			permissionNames.push(item.key);
-			let li = document.createElement('li');
-			let label = document.createElement('label');
-			label.innerHTML = item.key;
-			label.setAttribute('for', item.key);
-			let valueList = document.createElement('select');
-			valueList.id = valueList.name = item.key;
-			let allowOption = document.createElement('option');
-			allowOption.innerHTML = allowOption.value = 'allow';
-			let ignoreOption = document.createElement('option');
-			ignoreOption.innerHTML = ignoreOption.value = 'ignore';
-			let denyOption = document.createElement('option');
-			denyOption.innerHTML = denyOption.value = 'deny';
-			let defaultOption = document.createElement('option');
-			defaultOption.innerHTML = defaultOption.value = 'default';
-			switch (item.value){
-				case 'allow':
-					allowOption.selected = 'selected';
-					break;
-				case 'ignore':
-					ignoreOption.selected = 'selected';
-					break;
-				case 'deny':
-					denyOption.selected = 'selected';
-					break;
-				default:
-					defaultOption.selected = 'selected';
-			}
-			valueList.appendChild(allowOption);
-			valueList.appendChild(ignoreOption);
-			valueList.appendChild(defaultOption);
-			li.appendChild(label);
-			li.appendChild(valueList);
-			ul.appendChild(li);
-		});
-		let btn = document.createElement('button');
-		btn.type = 'submit';
-		btn.innerHTML = '设置';
-		form.appendChild(ul);
-		form.appendChild(btn);
-		
-		btn.addEventListener('click', function(e){
-			e.preventDefault();
-			let data = [];
-			permissionNames.forEach(function(item, index, arr){
-				let obj = {
-					key: item,
-					value: form[item].value
-				};
-				data.push(obj);
-			});
-			ipcRenderer.send('setAppPermissions', {
-				packageName: packageName,
-				permissions: data
-			});
-		});
-	}else{
-		let tip = document.createElement('p');
-		tip.innerHTML = 'This app has no permission.';
-		form.appendChild(tip);
-	}
-	container.appendChild(form);
-	container.className = 'app open';
-}
-
-ipcRenderer.on('getAppPermissions', function(e, arg){
-	if(arg.code){
-		renderAppPermissions(arg.packageName, arg.permissions);
-	}else{
-		console.log(arg.err);
-	}
-});
-
-ipcRenderer.on('getAppList', function(e, arg){
-	if(arg.code){
-		renderAppList(arg.data);
-	}else{
-		console.log(arg.err);
-	}
-});
-
-
-// init
-ipcRenderer.send('getAppList');
-
-// refresh
-let btnRefresh = document.getElementById('btnRefresh');
-btnRefresh.addEventListener('click', function(e){
-	let contents = document.getElementById('appList').childNodes;
-	for (let i = contents.length - 1; i >= 0; i--) {
-		document.getElementById('appList').removeChild(contents[i]);
-	}
-	ipcRenderer.send('getAppList');
-});
